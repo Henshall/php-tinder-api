d25941a9d061d8322689a09450437db764cc63d0
Grigor Grigoryan <grigor@upsoftware.co.uk>
2020-10-06 12:22:01 +0400

    improvements

--- src/TinderApi.php
+++ src/TinderApi.php
@@ -27,12 +27,8 @@ class TinderApi implements TinderApiInterface
      */
     public function getProfile($token)
     {
-        $response = $this->client->get(self::URL . '/profile', [
-            'headers' => [
-                'X-Auth-Token' => $token,
-            ]
-        ]);
-        return json_decode($response->getBody()->getContents(), true);
+
+        return $this->makeGetRequest($token, '/profile');
     }
 
     /**
@@ -40,36 +36,27 @@ class TinderApi implements TinderApiInterface
      *
      * @param string $token
      *
-     * @return array
+     * @return mixed
      */
     public function getRecommendations($token)
     {
-        $response = $this->client->get(self::URL . '/v2/recs/core', [
-            'headers' => [
-                'X-Auth-Token' => $token
-            ]
-        ]);
 
-        return json_decode($response->getBody()->getContents(), true);
+        return $this->makeGetRequest($token, '/v2/recs/core');
     }
 
     /**
      * Swipe to right.
      *
      * @param string $token
+     *
      * @param string $id
      *
      * @return mixed
      */
     public function like($token, $id)
     {
-        $response = $this->client->get(self::URL . '/like/' . $id, [
-            'headers' => [
-                'X-Auth-Token' => $token
-            ]
-        ]);
 
-        return json_decode($response->getBody()->getContents(), true);
+        return $this->makeGetRequest($token, '/like/' . $id);
     }
 
 
@@ -77,19 +64,15 @@ class TinderApi implements TinderApiInterface
      * Swipe left.
      *
      * @param string $token
+     *
      * @param string $id
      *
      * @return mixed
      */
     public function pass($token, $id)
     {
-        $response = $this->client->get(self::URL . '/pass/' . $id, [
-            'headers' => [
-                'X-Auth-Token' => $token
-            ]
-        ]);
 
-        return json_decode($response->getBody()->getContents(), true);
+        return $this->makeGetRequest($token, '/pass/' . $id);
     }
 
 
@@ -97,19 +80,13 @@ class TinderApi implements TinderApiInterface
      * GETS YOUR OWN METADATA.
      *
      * @param string $token
-     * @param string $id
      *
      * @return mixed
      */
     public function getMetadata($token)
     {
-        $response = $this->client->get(self::URL . '/v2/meta', [
-            'headers' => [
-                'X-Auth-Token' => $token
-            ]
-        ]);
 
-        return json_decode($response->getBody()->getContents(), true);
+        return $this->makeGetRequest($token, '/v2/meta/');
     }
 
 
@@ -123,14 +100,7 @@ class TinderApi implements TinderApiInterface
      */
     public function ping($token, array $position)
     {
-        $response = $this->client->post(self::URL . '/user/ping', [
-            'json' => $position,
-            'headers' => [
-                'X-Auth-Token' => $token
-            ]
-        ]);
-
-        $data = json_decode($response->getBody()->getContents(), true);
+        $data = $this->makeGetRequest($token, '/user/ping');
 
         if (array_key_exists('error', $data)) {
             throw new \RuntimeException('You can`t change your location frequently. Please try again later.');
@@ -149,13 +119,7 @@ class TinderApi implements TinderApiInterface
      */
     public function getTokenFromRefreshToken($token)
     {
-        $response = $this->client->post(self::URL . '/v2/auth/login/sms', [
-            'json' => [
-                'refresh_token' => $token,
-            ]
-        ]);
-
-        $responseArray = json_decode($response->getBody()->getContents(), true);
+        $responseArray = $this->makeGetRequest($token, '/v2/auth/login/sms');
 
         return $responseArray['data'];
     }
@@ -165,9 +129,9 @@ class TinderApi implements TinderApiInterface
      *
      * @param string $phoneNumber Your phone number associated with your tinder account
      *
-     * @return object
+     * @throws
      *
-     * @NOTE: for some reason this request needed to be done with curl. I could not get it to work with guzzle. Update for consistency.
+     * @return object
      */
     public function validateCode($phoneNumber, $code)
     {
@@ -200,6 +164,7 @@ class TinderApi implements TinderApiInterface
         } catch (\Exception $e) {
             return $e->getMessage();
         }
+
         $responseArray = json_decode($response->getBody()->getContents(), true);
 
         return $responseArray['data'];
@@ -210,125 +175,215 @@ class TinderApi implements TinderApiInterface
      *
      * @param string $phoneNumber Your phone number associated with your tinder account
      *
-     * @return string
+     * @throws
      *
-     * @NOTE: for some reason this request needed to be done with curl. I could not get it to work with guzzle. Update for consistency.
+     * @return string
      */
+
+
     public function requestCode($phoneNumber)
     {
         $number_plus = $phoneNumber;
         $number = ltrim($number_plus, '+');
-        $number = preg_replace("/[^0-9]/", "", $number);
+        $number = preg_replace("/[^0-9]/", "", $number );
 
-        $headers = [
-            "Authority" => " api.gotinder.com",
-            "Pragma" => " no-cache",
-            "Cache-Control" => " no-cache",
-            "X-Supported-Image-Formats" => " webp,jpeg",
-            "Funnel-Session-Id" => " bb041b36638ca491",
-            "Persistent-Device-Id" => " 7dd5826b-8433-4543-bb4b-c9937e8c4205",
-            "Tinder-Version" => " 2.43.0",
-            "User-Agent" => " Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
-            "Content-Type" => " application/x-google-protobuf",
-            "User-Session-Id" => " null",
-            "Accept" => " application/json",
-            "App-Session-Time-Elapsed" => " 14852",
-            "X-Auth-Token" => " ",
-            "User-Session-Time-Elapsed" => " null",
-            "Platform" => " web",
-            "App-Session-Id" => " 93c554f5-f39c-4cf7-845c-4989cb321e1b",
-            "App-Version" => " 1024300",
-            "Origin" => " https",
-            "Sec-Fetch-Site" => " cross-site",
-            "Sec-Fetch-Mode" => " cors",
-            "Sec-Fetch-Dest" => " empty",
-            "Referer" => " https",
-            "Accept-Language" => " en-US,en;q=0.9,fr;q=0.8"
-        ];
+        $ch = curl_init();
 
-        try {
-            $response = $this->client->post(self::URL . '/v3/auth/login?locale=en', [
-                'body' => chr(10) . chr(13) . chr(10) . chr(11) . $number,
-                'headers' => $headers
-            ]);
-        } catch (\Exception $e) {
-            return $e->getMessage();
+        curl_setopt($ch, CURLOPT_URL, 'https://api.gotinder.com/v3/auth/login?locale=en');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, chr(10).chr(13).chr(10).chr(11).$number);
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
+        $headers = array();
+        $headers[] = 'Authority: api.gotinder.com';
+        $headers[] = 'Pragma: no-cache';
+        $headers[] = 'Cache-Control: no-cache';
+        $headers[] = 'X-Supported-Image-Formats: webp,jpeg';
+        $headers[] = 'Funnel-Session-Id: bb041b36638ca491';
+        $headers[] = 'Persistent-Device-Id: 7dd5826b-8433-4543-bb4b-c9937e8c4205';
+        $headers[] = 'Tinder-Version: 2.43.0';
+        $headers[] = 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1';
+        $headers[] = 'Content-Type: application/x-google-protobuf';
+        $headers[] = 'User-Session-Id: null';
+        $headers[] = 'Accept: application/json';
+        $headers[] = 'App-Session-Time-Elapsed: 14852';
+        $headers[] = 'X-Auth-Token: ';
+        $headers[] = 'User-Session-Time-Elapsed: null';
+        $headers[] = 'Platform: web';
+        $headers[] = 'App-Session-Id: 93c554f5-f39c-4cf7-845c-4989cb321e1b';
+        $headers[] = 'App-Version: 1024300';
+        $headers[] = 'Origin: https://tinder.com';
+        $headers[] = 'Sec-Fetch-Site: cross-site';
+        $headers[] = 'Sec-Fetch-Mode: cors';
+        $headers[] = 'Sec-Fetch-Dest: empty';
+        $headers[] = 'Referer: https://tinder.com/';
+        $headers[] = 'Accept-Language: en-US,en;q=0.9,fr;q=0.8';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
         }
-
-        return $response->getBody()->getContents();
+        curl_close($ch);
+        $result = preg_replace("/[^0-9]/", "", $result );
+        return $result;
     }
 
+
+
+//    public function requestCode($phoneNumber)
+//    {
+//        $number_plus = $phoneNumber;
+//        $number = ltrim($number_plus, '+');
+//        $number = preg_replace("/[^0-9]/", "", $number);
+//
+//        $headers = [
+//            "Authority" => " api.gotinder.com",
+//            "Pragma" => " no-cache",
+//            "Cache-Control" => " no-cache",
+//            "X-Supported-Image-Formats" => " webp,jpeg",
+//            "Funnel-Session-Id" => " bb041b36638ca491",
+//            "Persistent-Device-Id" => " 7dd5826b-8433-4543-bb4b-c9937e8c4205",
+//            "Tinder-Version" => " 2.43.0",
+//            "User-Agent" => " Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+//            "Content-Type" => " application/x-google-protobuf",
+//            "User-Session-Id" => " null",
+//            "Accept" => " application/json",
+//            "App-Session-Time-Elapsed" => " 14852",
+//            "X-Auth-Token" => " ",
+//            "User-Session-Time-Elapsed" => " null",
+//            "Platform" => " web",
+//            "App-Session-Id" => " 93c554f5-f39c-4cf7-845c-4989cb321e1b",
+//            "App-Version" => " 1024300",
+//            "Origin" => " https",
+//            "Sec-Fetch-Site" => " cross-site",
+//            "Sec-Fetch-Mode" => " cors",
+//            "Sec-Fetch-Dest" => " empty",
+//            "Referer" => " https",
+//            "Accept-Language" => " en-US,en;q=0.9,fr;q=0.8"
+//        ];
+//
+//        try {
+//            $response = $this->client->post(self::URL . '/v3/auth/login?locale=en', [
+//                'body' => chr(10) . chr(13) . chr(10) . chr(11) . $number,
+//                'headers' => $headers
+//            ]);
+//        } catch (\Exception $e) {
+//            return $e->getMessage();
+//        }
+//
+//        return $response->getBody()->getContents();
+//    }
+
     /**
-     * @param $token
-     * @param $profileId
+     * @param string $token
+     * @param string $profileId
+     *
      * @return mixed
-     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function getUser($token, $profileId)
     {
-        $url = self::URL . '/user/' . $profileId;
-        $response = $this->client->get($url, [
-            'headers' => [
-                'X-Auth-Token' => $token,
-            ]
-        ]);
 
-        return json_decode($response->getBody()->getContents(), true);
+        return $this->makeGetRequest($token, '/user/' . $profileId);
     }
 
 
+    /**
+     * Get matches profiles.
+     *
+     * @param string $token
+     *
+     * @return mixed
+     */
     public function getMatches($token)
     {
-        $response = $this->client->get(self::URL . '/v2/matches?count=50', [
-            'headers' => [
-                'X-Auth-Token' => $token,
-            ]
-        ]);
 
-        return json_decode($response->getBody()->getContents(), true);
-
+        return $this->makeGetRequest($token, '/v2/matches?count=50');
     }
 
+    /**
+     * Get certain matched profile by id.
+     *
+     * @param string $token
+     *
+     * @param string $matchId
+     *
+     * @return mixed
+     */
     public function getCertainMatch($token, $matchId)
     {
-        $response = $this->client->get(self::URL . "/matches/{$matchId}", [
-            'headers' => [
-                'X-Auth-Token' => $token,
-            ]
-        ]);
 
-        return json_decode($response->getBody()->getContents(), true);
-
+        return $this->makeGetRequest($token, "/matches/{$matchId}");
     }
 
+    /**
+     * Get common connection of a user
+     *
+     * @param string $token
+     *
+     * @param string $userId
+     *
+     * @return mixed
+     */
     public function getCommonConnections($token, $userId)
     {
-        $response = $this->client->get(self::URL . "/user/{$userId}/common_connections", [
-            'headers' => [
-                'X-Auth-Token' => $token,
-            ]
-        ]);
 
-        return json_decode($response->getBody()->getContents(), true);
+        return $this->makeGetRequest($token, "/user/{$userId}/common_connections");
     }
 
+    /**
+     * Get Spotify settings
+     *
+     * @param string $token
+     *
+     * @return mixed
+     */
     public function getSpotifySettings($token)
     {
-        $response = $this->client->get(self::URL . '/v2/profile/spotify', [
-            'headers' => [
-                'X-Auth-Token' => $token,
-            ]
+
+        return $this->makeGetRequest($token, '/v2/profile/spotify');
+    }
+
+    /**
+     * Send Message to that id
+     *
+     * @param string $token
+     *
+     * @param string $userId
+     *
+     * @param string $message
+     *
+     * @throws
+     *
+     * @return array
+     */
+    public function sendMessage($token, $userId, $message)
+    {
+        //$userId = '5f44702123434afe8d';
+        $response = $this->client->post(self::URL . "/user/matches/{$userId}/", [
+            'json' => ['message' => $message],
         ]);
 
         return json_decode($response->getBody()->getContents(), true);
-
     }
 
-    public function sendMessage($token, $userId, $message)
+    /**
+     * Common method to make some get requests
+     *
+     * @param string $token
+     *
+     * @param string $url
+     *
+     * @throws
+     *
+     * @return array
+     */
+    private function makeGetRequest($token, $url)
     {
-        $userId = '5f44702123434afe8d';
-        $response = $this->client->post(self::URL . "/user/matches/{$userId}/", [
-            'json' => ['message' => $message],
+        $response = $this->client->get(self::URL . $url, [
+            'headers' => [
+                'X-Auth-Token' => $token,
+            ]
         ]);
 
         return json_decode($response->getBody()->getContents(), true);
